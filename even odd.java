4e92718db8fc0b5318ae1208d20ff96c769e9137
Navya Sree <navyasree9490@gmail.com>
2026-03-25 11:18:26 +0530

    Fix class name to follow Java naming conventions

--- even odd.java
+++ even odd.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
 
-public class EvenOddChecker {
+
+public class even odd {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number to check: ");
